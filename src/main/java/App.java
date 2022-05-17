ae185e34020cbedc1643aa022cf86429484f535d
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-18 01:47:23 +0300

    Updated layouts

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,26 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.get;
+
+
+
+import static java.lang.reflect.Array.get;
+import static spark.Spark.staticFileLocation;
+import static spark.route.HttpMethod.get;
+
+
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("N-tiko Initilizing....");
+        get("/", (request, response)->{
+           return new ModelAndView(new HashMap<>(),"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
 
     }
 }
