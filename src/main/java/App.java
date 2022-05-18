017f801cecfdcd3b53477c07e265d82d6b7c814f
themaryanjuguna <themaryanjuguna@gmail.com>
2022-05-18 03:11:09 +0300

    multiple hbs pages created /updated

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,18 +3,16 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import static spark.Spark.get;
-
-
-
-import static java.lang.reflect.Array.get;
 import static spark.Spark.staticFileLocation;
-import static spark.route.HttpMethod.get;
 
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
 
+        Map<String, Object> model = new HashMap<>();
         get("/", (request, response)->{
            return new ModelAndView(new HashMap<>(),"index.hbs");
         }, new HandlebarsTemplateEngine());
